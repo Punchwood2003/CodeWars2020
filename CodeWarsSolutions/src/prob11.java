c8f00304bb681de8858cffb8688ca7fa5409d13a
ANANYA <subramaniama@go.lisd.net>
2020-05-09 17:29:29 -0500

    prob11

--- CodeWarsSolutions/src/prob11.java
+++ CodeWarsSolutions/src/prob11.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 
 public class prob11
 {
-
+	public static void main(String [] args)
+	{
+		Scanner s= new Scanner(System.in);
+		while(s.hasNext())
+		{
+			int num = s.nextInt();
+			String binary = Integer.toBinaryString(num);
+			if(binary.length() % 2 == 1 &&
+			  (binary.indexOf('0') == binary.lastIndexOf('0') &&
+			  (binary.charAt(binary.length() / 2) == '0')))
+			{
+				System.out.println(num + " yes");
+			}
+			else
+			{
+				System.out.println(num + " no");
+			}
+		}
+		
+	}
 }
