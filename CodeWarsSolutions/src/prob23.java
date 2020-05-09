924af26d43dd45861b8f46af1aaf7a9a073366b6
Punchwood2003 <punchwood2003@gmail.com>
2020-05-09 17:50:41 -0500

    Completed prob23

--- CodeWarsSolutions/src/prob23.java
+++ CodeWarsSolutions/src/prob23.java
@@ -1,5 +1,84 @@
+import java.util.Scanner;
+
 // author@MatthewSheldon
 public class prob23 {
-	public static void main(String[] args) {}
-	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int numTimes = scan.nextInt(); scan.nextLine();
+		for(int i = 0; i < numTimes; i++) {
+			String temp = scan.nextLine().toLowerCase().replaceAll("[,.?:!'\";]", "");
+			String mirror = ""; 
+			for(int j = temp.length()-1; j >= 0 ; j--) {
+				mirror += temp.charAt(j);
+			}
+			String rot = "";
+			for(int j = 0; j < mirror.length(); j++) {
+				rot += getComplement(mirror.charAt(j));
+			}
+			
+			String work = "not";
+			String compare1 = temp.replaceAll(" ", "");
+			String compare2 = rot.replaceAll(" ", "");
+			if(compare1.compareTo(compare2) == 0) {
+				work = "is";
+			}
+			
+			System.out.println(temp + " (" + work + ") " + rot);
+		}
+	}
+	public static char getComplement(char curr) {
+		char rot = curr;
+		switch(curr) {
+			case 'a' : {
+				rot = 'e';
+				break;
+			}
+			case 'e' : {
+				rot = 'a';
+				break;
+			}
+			case 'b' : {
+				rot = 'q';
+				break;
+			}
+			case 'q' : {
+				rot = 'b';
+				break;
+			}
+			case 'd' : {
+				rot = 'p';
+				break;
+			}
+			case 'p' : {
+				rot = 'd';
+				break;
+			}
+			case 'h' : {
+				rot = 'y';
+				break;
+			}
+			case 'y' : {
+				rot = 'h';
+				break;
+			}
+			case 'm' : {
+				rot = 'w';
+				break;
+			}
+			case 'w' : {
+				rot = 'm';
+				break;
+			}
+			case 'n' : {
+				rot = 'u';
+				break;
+			}
+			case 'u' : {
+				rot = 'n';
+				break;
+			}
+			default : {break;}
+		}
+		return rot;
+	}
 }
